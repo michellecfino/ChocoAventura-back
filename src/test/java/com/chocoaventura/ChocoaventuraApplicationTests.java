d9022c3966c26c8d5c1644cb30b092a4354268f0
Laura Fernanda Martinez Galindo <lf.martinezg1@uniandes.edu.co>
2026-03-20 20:21:47 -0500

    Crear proyecto

---
+++ src/test/java/com/chocoaventura/ChocoaventuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocoaventura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocoaventuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
