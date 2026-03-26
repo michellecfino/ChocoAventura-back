8626b9e2f43deff2be6e9d312288c475b9f8998c
Catalina Villacris <m.villacris@uniandes.edu.co>
2026-03-26 15:59:57 -0500

    actualización rama principal

---
+++ src/main/java/com/chocoaventura/services/CaculadoraGastos.java
@@ -0,0 +1,15 @@
+package com.chocoaventura.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CaculadoraGastos {
+
+    @Autowired
+    private DeudaService deudaService;
+
+    @Autowired
+    private PagoService pagoService;
+
+    
+    
+}
